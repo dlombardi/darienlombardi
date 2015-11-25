efc053d1e2fac98ea69c795b6a6f312825e7ecbe
Darien Lombardi <darien.lombardi@gmail.com>
2015-11-24 22:02:23 -0800

    all sections layed out. email not functional

--- js/portfolio.js
+++ js/portfolio.js
@@ -9,10 +9,38 @@
 // jQuery to collapse the navbar on scroll
 
 function init() {
-  $(window).scroll(function() {
-      ($(".navbar").offset().top > 50) ? $(".navbar-fixed-top").addClass("top-nav-collapse") : $(".navbar-fixed-top").removeClass("top-nav-collapse");
+
+  var $animation_elements = [$('#portfolioHeader'), $('#aboutHeader')];
+  var $window = $(window);
+
+  $window.on('scroll', check_if_in_view);
+  $window.on('scroll resize', check_if_in_view);
+  $window.trigger('scroll');
+
+  $window .scroll(function() {
+    ($(".navbar").offset().top > 50) ? $(".navbar-fixed-top").addClass("top-nav-collapse") : $(".navbar-fixed-top").removeClass("top-nav-collapse");
   });
 
+  function check_if_in_view() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+    $.each($animation_elements, function() {
+      var $element = $(this);
+      var element_height = $element.outerHeight();
+      var element_top_position = $element.offset().top;
+      var element_bottom_position = (element_top_position + element_height);
+
+      //check to see if this current container is within viewport
+      if ((element_bottom_position >= window_top_position) &&
+          (element_top_position <= window_bottom_position)) {
+        $element.addClass('animated fadeInLeft');
+      } else {
+        $element.removeClass('animated FadeInLeft');
+      }
+    });
+  }
   // jQuery for page scrolling feature - requires jQuery Easing plugin
   $(function() {
     $('a.page-scroll').bind('click', function(event) {
