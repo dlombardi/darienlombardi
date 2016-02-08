4b2bd996edf47cec929791fea290f4f0b974f74e
Darien Lombardi <darien.lombardi@gmail.com>
2016-02-08 11:05:23 -0500

    add vinbeam project

--- public/js/ngPortfolio.js
+++ public/js/ngPortfolio.js
@@ -9,10 +9,10 @@ dl.controller("dController", ['$scope', '$http', function($scope, $http){
   ];
 
   $scope.portfolio = [
+    { title:"VINBeam website", image: "/public/images/Vinbeam.png", website: "http://www.vinbeam.com/", github: "https://github.com/dlombardi/Quora_clone" ,techs:["Node","Javascript","Bootstrap","Angular","Express","Scss","Gulp"]},
     { title:"Quora", image: "/public/images/quora.png", website: "https://blooming-dusk-5310.herokuapp.com/", github: "https://github.com/dlombardi/Quora_clone" ,techs:["Node","Javascript","Angular","Express","Mongoose","Scss","Gulp", "Mixpanel", "Passport","Foundation"]},
     { title:"Historic (Mobile)", image: "/public/images/Historic.png", website: "https://radiant-island-9611.herokuapp.com/#/app/map", github: "https://github.com/dlombardi/historic" ,techs:["Node","Javascript","Ionic","Google Maps Api","Express","Mongo","Mongoose","Scss","Gulp"]},
     { title:"Google Places Search", image: "/public/images/google_places_search.png", website: "http://placessearch.gq", github: "https://github.com/dlombardi/Google-Places-SearchTool" ,techs:["Angular","Gulp","Google materialize","Google maps places library"]},
-    { title:"PocketWatch", image: "/public/images/pocket_watch.png", website: "http://www.pocketwatchapp.us/#/home", github: "https://github.com/dlombardi/pocketwatchapp", techs:["Javascript","Twilio", "Weather Underground API","Express","HTML", "CSS"]},
     { title:"Weather App", image: "/public/images/WeatherApp.png", website: "http://dlombardi.github.io/Weather-on-the-Run/", github: "https://github.com/dlombardi/Weather-on-the-Run",techs:["Javascript","jQuery", "Weather Underground API","Bootstrap","HTML","CSS"]},
     { title:"Calculator", image: "/public/images/Calculator.png", website: "http://dlombardi.github.io/Calculator/", github: "https://github.com/dlombardi/Calculator",techs:["Javascript", "jQuery", "Bootstrap", "HTML", "CSS"]}
   ]
